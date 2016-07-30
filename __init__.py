45956d6f89acd25cfcba27a547fd102da14cedc6
Craig Jones <artistcdmj@gmail.com>
2016-07-30 14:05:26 -0500

    Update __init__.py
    Added 4 ops from Test Panel sandbox work: Difference Mask Bool, Union Mask Bool, Reproject Mask, and Remove Modifier.
    Will probably need Spirou4D to proof my work on these :D

--- __init__.py
+++ __init__.py
@@ -23,7 +23,7 @@
 
 bl_info = {"name": "Artist Paint Panel",
            "author": "CDMJ, Spirou4D",
-           "version": (1, 00, 0),
+           "version": (2, 00, 0),
            "blender": (2, 77, 0),
            "location": "Toolbar > Misc Tab > Artist Panel",
            "description": "Artist Paint Studio.",
@@ -1766,6 +1766,161 @@ class CanvasResetrot(Operator):
             scene.locking_are_desactived = False #init the locking state
         return {'FINISHED'}
 
+#########################################################experimental operations
+
+class ReprojectMask(bpy.types.Operator):
+    """Reproject Mask"""
+    bl_idname = "artist_paint.reproject_mask" 
+                                     
+     
+    bl_label = "Reproject Mask by View"
+    bl_options = { 'REGISTER', 'UNDO' }
+    
+    def execute(self, context):
+
+        scene = context.scene
+
+
+        #new code
+        bpy.ops.object.editmode_toggle() #toggle edit mode
+        bpy.ops.uv.project_from_view(camera_bounds=True, correct_aspect=False, scale_to_bounds=False) #project from view
+        bpy.ops.object.editmode_toggle() #toggle back from edit mode
+        bpy.ops.object.convert(target='MESH')#in obj mode, convert to mesh for correction on Artist Panel Vector Masks/Gpencil Masks
+
+        bpy.ops.paint.texture_paint_toggle() #toggle texpaint
+        return {'FINISHED'}
+    
+#next operator
+class SolidfyDifference(bpy.types.Operator):
+    """Solidify and Difference Mask"""
+    bl_idname = "artist_paint.solidfy_difference"
+    bl_label = "Add Solidy and Difference Bool"
+    bl_options = { 'REGISTER','UNDO' }
+    
+    def execute(self, context):
+        scene = context.scene
+                
+        
+        #new code
+        sel = bpy.context.selected_objects
+        act = bpy.context.scene.objects.active
+        
+        for obj in sel:
+            context.scene.objects.active = obj#set active to selected
+            bpy.ops.object.editmode_toggle()
+            bpy.ops.mesh.dissolve_faces()#to get a clean single face for paint projection
+            bpy.ops.object.editmode_toggle()
+
+            bpy.ops.object.modifier_add(type='SOLIDIFY')#set soldifiy for bool
+            bpy.context.object.modifiers["Solidify"].thickness = 0.3#thicker than active
+            bpy.ops.transform.translate(value=(0, 0, 0.01), constraint_axis=(False, False, True), constraint_orientation='GLOBAL', mirror=False, proportional='DISABLED', proportional_edit_falloff='SMOOTH', proportional_size=1, release_confirm=True)#attempt to only move bool brush up in Z
+            
+            context.scene.objects.active = act#reset active 
+            
+            bpy.ops.object.editmode_toggle()
+            bpy.ops.mesh.dissolve_faces()
+            bpy.ops.object.editmode_toggle()
+
+            
+            bpy.ops.object.modifier_add(type='SOLIDIFY')#basic soldify for boolean
+            bpy.ops.transform.translate(value=(0, 0, 0), constraint_axis=(False, False, True), constraint_orientation='GLOBAL', mirror=False, proportional='DISABLED', proportional_edit_falloff='SMOOTH', proportional_size=1, release_confirm=True)#to move active 0 in Z
+                
+            
+            bpy.ops.btool.boolean_diff()#call booltool
+                
+            
+            return {'FINISHED'}           
+
+        
+        #user moves parent and then adjusts child
+        #user calls reproject while in obj mode which returns mask to texpaint
+        
+#next operator
+class SolidfyUnion(bpy.types.Operator):
+    """Solidify and Union Mask"""
+    bl_idname = "artist_paint.solidfy_union"
+    bl_label = "Add Solidy and Union Bool"
+    bl_options = { 'REGISTER','UNDO' }
+    
+    def execute(self, context):
+        scene = context.scene
+                
+        
+        #new code
+        sel = bpy.context.selected_objects
+        act = bpy.context.scene.objects.active
+        
+        for obj in sel:
+            context.scene.objects.active = obj#set active to selected
+            
+            bpy.ops.object.editmode_toggle()
+            bpy.ops.mesh.dissolve_faces()#to get a single face for paint projection
+            bpy.ops.object.editmode_toggle()
+            
+            bpy.ops.object.modifier_add(type='SOLIDIFY')#set soldifiy for bool
+            bpy.context.object.modifiers["Solidify"].thickness = 0.3#thicker than active
+            #bpy.ops.transform.translate(value=(0, 0, 0.01), constraint_axis=(False, False, True), constraint_orientation='GLOBAL', mirror=False, proportional='DISABLED', proportional_edit_falloff='SMOOTH', proportional_size=1, release_confirm=True)#attempt to only move bool brush up in Z
+            
+            context.scene.objects.active = act#reset active 
+            
+            bpy.ops.object.editmode_toggle()
+            bpy.ops.mesh.dissolve_faces()
+            bpy.ops.object.editmode_toggle()
+            
+            bpy.ops.object.modifier_add(type='SOLIDIFY')#basic soldify for boolean
+            bpy.context.object.modifiers["Solidify"].thickness = 0.3#thicker than active
+            #bpy.ops.transform.translate(value=(0, 0, 0), constraint_axis=(False, False, True), constraint_orientation='GLOBAL', mirror=False, proportional='DISABLED', proportional_edit_falloff='SMOOTH', proportional_size=1, release_confirm=True)#to move active 0 in Z
+                
+            
+            bpy.ops.btool.boolean_union()#call booltool
+                
+            
+            return {'FINISHED'}           
+
+        
+        #user moves parent and then adjusts child
+        #user calls reproject while in obj mode which returns mask to texpaint
+        
+        
+#next operator
+class RemoveMods(bpy.types.Operator):
+    """Remove Modifiers"""
+    bl_idname = "artist_paint.remove_modifiers"
+    bl_label = "Remove Modifiers"
+    bl_options = { 'REGISTER','UNDO' }
+    
+    def execute(self, context):
+        scene = context.scene
+                
+        
+        #new code
+        context = bpy.context
+        scene = context.scene
+        obj = context.object
+
+        # get a reference to the current obj.data
+        old_mesh = obj.data
+
+        # settings for to_mesh
+        apply_modifiers = False
+        settings = 'PREVIEW'
+        new_mesh = obj.to_mesh(scene, apply_modifiers, settings)
+
+        # object will still have modifiers, remove them
+        obj.modifiers.clear()
+
+        # assign the new mesh to obj.data 
+        obj.data = new_mesh
+
+        # remove the old mesh from the .blend
+        bpy.data.meshes.remove(old_mesh)
+        bpy.context.object.draw_type = 'TEXTURED'
+
+        
+                
+            
+        return {'FINISHED'}
+
 
 
 ##############################################################  panel
@@ -1943,6 +2098,27 @@ class ArtistPanel(Panel):
             row.prop(ipaint, "invert_stencil",
                         text="Invert the mask",
                         icon='IMAGE_ALPHA')
+    
+        col.separator()
+    
+        box = layout.box()                        #BOOL MASK AND REUSE
+        col = box.column(align = True)
+        row = col.row(align = True)
+        row1 = row.split(align=True)
+        row1.label(text="Bool")
+        row1.scale_x = 0.50
+        row.separator()
+        row2 = row.split(align=True)
+        row2.operator("artist_paint.solidfy_difference", text="Difference", icon = 'ROTACTIVE')
+        row2.operator("artist_paint.solidfy_union", text="Union", icon = 'ROTATECOLLECTION')
+        row2.scale_x = 1.00
+        row.separator()
+        row3 = row.split(align=True)
+        row3.operator("artist_paint.reproject_mask",
+                    text="Reproject", icon = 'NODE_SEL')
+        row4 = row.split(align=True)
+        row4.operator("artist_paint.remove_modifiers",
+                    text="", icon='RECOVER_LAST')
 
 
         box = layout.box()                        #CANVAS FRAME CONSTRAINT
